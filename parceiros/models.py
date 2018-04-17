53515a7561974a969ad6943d9011a509843e26b6
Ezequiel <equeirozdenoronha@gmail.com>
2018-04-16 21:33:19 -0300

    Commit Post Parceiro

--- parceiros/models.py
+++ parceiros/models.py
@@ -5,10 +5,9 @@ from django.contrib.auth.models import User
 
 
 class Parceiro(models.Model):
-    nome_empresa = models.IntegerField(max_length=200)
+    nome_empresa = models.CharField(max_length=200)
     nome = models.CharField(max_length=250)
     email = models.CharField(max_length=300)
     telefone = models.CharField(max_length=30)
     proposta = models.CharField(max_length=350)
     tipo = models.CharField(max_length=40)
-
--- parceiros/views.py
+++ parceiros/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.http import JsonResponse
 from parceiros.models import *
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 import json
 
@@ -10,14 +11,20 @@ import json
 def cadastro(request):
     """Tela de cadastro do parceiro"""
     if request.method == 'POST':
+
         print(request.body)
-        data = json.loads(request.body)
+        __parceiro = Parceiro
+
+        data = json.loads(request.body.decode('utf-8'))
         nome_empresa = data['nome_empresa']
         nome = data['nome']
         email = data['email']
         telefone = data['telefone']
         proposta = data['proposta']
         tipo = data['tipo']
+        __parceiro = Parceiro(nome_empresa=nome_empresa,nome=nome,email=email,telefone=telefone,proposta=proposta,tipo=tipo)
+        __parceiro.save()
+
         return JsonResponse({'mensagem':'Registrado com sucesso'}, safe=False)
 
     return JsonResponse({'mensagem':'Erro'} , safe=False)
